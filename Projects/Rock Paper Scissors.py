ede2cabd9ee906310b6dd2a7e6ab8592209a6614
Anshul Kumar <anshustudy21@gmail.com>
2025-07-28 16:24:58 +0530

    Update Rock Paper Scissors.py

--- Projects/Rock Paper Scissors.py
+++ Projects/Rock Paper Scissors.py
@@ -19,5 +19,33 @@ while True:
         continue
 
     random_number = random.randint(0,2)
+    # rock: 0, paper: 1, scissors: 2
+    # random_number will be used to select the computer's choice
+
     computer_pick = options[random_number]
-    print(f"Computer picked: {computer_pick}")
+    print("Computer picked", computer_pick + ".")
+
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You won!")
+        user_wins += 1
+        
+
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        user_wins += 1
+        
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == computer_pick:
+        print("It's a tie!")
+
+    else:
+        print("You lost!")
+        computer_wins += 1
+        
+print("You won", user_wins, "times.")
+print("The computer won", computer_wins, "times.")
+print("Goodbye!")
