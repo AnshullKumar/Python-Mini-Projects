1f5d44d690818433806e2cb677b6d1447992c1d2
AnshullKumar <anshustudy21@gmail.com>
2025-08-09 11:23:15 +0530

    new project

--- Projects/Timed_Math.py
+++ Projects/Timed_Math.py
@@ -1,12 +1,28 @@
+# This is a timed math practice program.
+# Here we take a set of random math problems and give the user a limited time to solve them.
+# The user can input their answers, and the program will keep track of how many attempts were wrong.
+# At the end, it will display the total time taken to solve all problems.
+
 import random
+import time
 
 operators = ["+", "-", "*"]
 min_operand = 3
 max_operand = 12
+total_problems = 10
 
 def generate_problem():
     left = random.randint(min_operand, max_operand)
     right = random.randint(min_operand, max_operand)
     operator = random.choice(operators)
 
-    exp = str(left + " " + operator + " " + str(operator))
+    exp = str(left) + " " + operator + " " + str(right)
+    answer = eval(exp) # eval() is used to calculate the result of the expression in string and give the output as an integer
+    return exp, answer
+
+wrong_attempts = 0
+input("Presss Enter to start! ")
+print("-------------------")
+
+start_time = time.time() # Record the start time
+
