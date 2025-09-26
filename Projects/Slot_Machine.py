b90ca3bff756f30d7280f7876f115ea2e2716278
AnshullKumar <anshustudy21@gmail.com>
2025-09-26 17:22:16 +0530

    added slot machine row display

--- Projects/Slot_Machine.py
+++ Projects/Slot_Machine.py
@@ -36,8 +36,14 @@ def get_slot_machine_spin(rows,cols,symbols):
     
     return columns
 
-#def print_slot_machine(columns):
-#    for row in range(len(columns[0])):
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end = " | ")
+            else:
+                print(column[row], end = "")
+        print() #new line by default
 
 
 def deposit():
@@ -96,5 +102,10 @@ def main():
             break
 
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+    
+    slots = get_slot_machine_spin(Rows,Cols,symbol_count)
+    print_slot_machine(slots)
+
+
 
 main()
