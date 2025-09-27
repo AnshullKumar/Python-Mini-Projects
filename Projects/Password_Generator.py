a75b21ae957a2278434fa2261c639c02b8181abc
AnshullKumar <anshustudy21@gmail.com>
2025-09-27 22:44:03 +0530

    Adding initial password generator function with support for letters, digits, and special characters

---
+++ Projects/Password_Generator.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+def generate_password(min_length, numbers = True, special_characters = True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation 
+
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special_characters
+
+    pwd = ""
+    meet_criteria = False
+    
+
+generate_password(10)
