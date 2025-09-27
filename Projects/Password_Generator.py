9f11a2c826526cb98f14793fdc5dcfd366dc3458
AnshullKumar <anshustudy21@gmail.com>
2025-09-28 00:14:16 +0530

    Improve password generator to enforce criteria until minimum length is met

--- Projects/Password_Generator.py
+++ Projects/Password_Generator.py
@@ -10,10 +10,31 @@ def generate_password(min_length, numbers = True, special_characters = True):
     if numbers:
         characters += digits
     if special_characters:
-        characters += special_characters
+        characters += special
 
     pwd = ""
     meet_criteria = False
-    
+    has_number = False
+    has_special = False
 
-generate_password(10)
+    while not meet_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if new_char in digits:
+            has_number = True
+        elif new_char in special:
+            has_special = True
+        
+        meet_criteria = True
+        if numbers:
+            meet_criteria = has_number
+        if special_characters:
+            meet_criteria = meet_criteria and has_special
+    return pwd
+
+min_Length = int(input("Enter the minimum length of your password: "))
+has_number = input("Do you want to have numbers (y/n)?: ").lower() == "y"
+has_special =  input("Do you want to hae special characters (y/n)?: ").lower() == "y"
+pwd = generate_password(min_Length, has_number, has_special)
+print("The generated password is: ", pwd)
