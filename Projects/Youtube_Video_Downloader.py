4a587f6f40cb8525fa3b1647982e8a5a05fa0d87
AnshullKumar <anshustudy21@gmail.com>
2025-12-19 01:15:31 +0530

    Add initial Youtube Video Downloader script

---
+++ Projects/Youtube_Video_Downloader.py
@@ -0,0 +1,14 @@
+
+
+# ------------------------
+# Youtube Video Downloader
+# ------------------------
+
+# Creating a simple Youtube video downloader ...
+# For personal use where Ill create a Terminal function to call this script ...
+# And save it in the .ps1 folder to have quick access for my ease of use ...
+# For example: 
+# If I call this is my terminal ...
+# DownloadYT "<youtube link>"
+
+from pytube import YouTube
