0fa45ccc5890a4fc7cef32e0eaed0faf42c6f9c6
AnshullKumar <anshustudy21@gmail.com>
2025-12-23 16:39:29 +0530

    YT Video Test Successful

--- Youtube_Video_Downloader.py
+++ Youtube_Video_Downloader.py
@@ -26,3 +26,10 @@ yt = YouTube(link)
 
 print("Title :", yt.title)
 print("Views :", yt.views)
+
+yd = yt.streams.get_highest_resolution()
+
+yd.download("X:\Downloads\Youtube Downloads")
+
+print("Downloaded Successfully into the specified directory...")
+
